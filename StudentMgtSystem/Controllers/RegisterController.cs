ac2f0a2cf625f3ed8e340ffe950acb3a72779e08
Haritha Gayashan <harithamx@gmail.com>
2017-08-06 20:15:51 +0530

    Add project files.

---
+++ StudentMgtSystem/Controllers/RegisterController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentMgtSystem.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        StudentContext db = new StudentContext();
+        public ActionResult reg()
+        {
+            return View(db.Stus.ToList());
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+      [HttpPost]
+      public ActionResult Create(Stu stu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Stus.Add(stu);
+                db.SaveChanges();
+                return RedirectToAction("reg");
+            }
+            return View(stu);
+        }
+        [HttpGet]
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Edit(Stu s)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Stus.Add(s);
+                db.SaveChanges();
+                return RedirectToAction("reg");
+            }
+            return View(s);
+        }
+        
+
+        }
+    }
